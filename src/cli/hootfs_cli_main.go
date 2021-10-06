173173520270eae3af403c606f76dd0e156c81c8
Landmaster <python0429@gmail.com>
2021-10-05 20:17:39 -0500

    add cli grpc connection code

--- src/cli/hootfs_cli_main.go
+++ src/cli/hootfs_cli_main.go
@@ -6,9 +6,12 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	head "github.com/hootfs/hootfs/protos"
+	"google.golang.org/grpc"
 )
 
-func executeCommand(args []string) {
+func executeCommand(args []string, rpcClient head.HootFsServiceClient) {
 	if len(args) == 0 {
 		return
 	}
@@ -21,17 +24,24 @@ func executeCommand(args []string) {
 }
 
 func main() {
-	var servername string
-	flag.StringVar(&servername, "s", "" /* todo set reasonable default */, "the uri of the head node to connect to")
+	var serverAddr string
+	flag.StringVar(&serverAddr, "s", "127.0.0.1", "the address of the head node to connect to")
 	flag.Parse()
 
-	// todo connect to head node
+	var grpcOpts []grpc.DialOption
+	conn, err := grpc.Dial(serverAddr, grpcOpts...)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	rpcClient := head.NewHootFsServiceClient(conn)
 
 	sc := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("hootfs> ")
 		sc.Scan()
 		cmd := sc.Text()
-		executeCommand(strings.Fields(cmd))
+		executeCommand(strings.Fields(cmd), rpcClient)
 	}
 }
