8b607bbafb1afe11e6aca669c5dc4e167fb30f7e
Landmaster <python0429@gmail.com>
2021-10-05 20:02:23 -0500

    add cli option skeleton

--- src/cli/hootfs_cli_main.go
+++ src/cli/hootfs_cli_main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func executeCommand(args []string) {
-	// TODO add
+	if len(args) == 0 {
+		return
+	}
+	switch args[0] {
+	case "read":
+		if len(args) != 2 {
+			fmt.Fprintln(os.Stderr, "usage: read [filename]")
+		}
+	}
 }
 
 func main() {
+	var servername string
+	flag.StringVar(&servername, "s", "" /* todo set reasonable default */, "the uri of the head node to connect to")
+	flag.Parse()
+
+	// todo connect to head node
+
 	sc := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("hootfs> ")
