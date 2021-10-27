b06bd20735a97e8848cab1551a6974da0053900a
chathamabate <chathamabate@gmail.com>
2021-10-27 01:26:04 -0500

    Work on Make Direcotry

--- src/core/head_node.go
+++ src/core/head_node.go
@@ -2,116 +2,133 @@ package core
 
 import (
 	"context"
-    "time"
-    "net"
-    "log"
+	"log"
+	"net"
+	"time"
 
+	uuid "github.com/google/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	"google.golang.org/grpc"
-	protos "github.com/hootfs/hootfs/protos"
 	head "github.com/hootfs/hootfs/protos"
-    discover "github.com/hootfs/hootfs/src/discover"
-    cluster "github.com/hootfs/hootfs/src/core/cluster"
-    hootfs "github.com/hootfs/hootfs/src/core/file_storage"
+	protos "github.com/hootfs/hootfs/protos"
+	cluster "github.com/hootfs/hootfs/src/core/cluster"
+	hootfs "github.com/hootfs/hootfs/src/core/file_storage"
+	discover "github.com/hootfs/hootfs/src/discover"
+	"google.golang.org/grpc"
 )
 
 const (
-    headPort = ":50060" 
-    nodePingDurr = 1 
-    nodeGetActiveDurr = 20
+	headPort          = ":50060"
+	nodePingDurr      = 1
+	nodeGetActiveDurr = 20
 )
 
-// File manager Server must deal with 
+// File manager Server must deal with
 // taking requests and pinging discovery server.
 
 type HootFsServer struct {
-    csc cluster.ClusterServiceClient
-    dc discover.DiscoverClient
-    
-    fmg *hootfs.FileManager
-    vfmg *hootfs.VirtualFileManager 
+	csc cluster.ClusterServiceClient
+	dc  discover.DiscoverClient
+
+	fmg  *hootfs.FileManager
+	vfmg *hootfs.VirtualFileManager
 
 	head.UnimplementedHootFsServiceServer
 }
 
-func NewFileManagerServer(dip string, fmg *hootfs.FileManager, 
-    vfmg *hootfs.VirtualFileManager) *HootFsServer {
-    return &HootFsServer{
-        dc: *discover.NewDiscoverClient(dip),
-        fmg: fmg,
-        vfmg: vfmg,
-    }
+func NewFileManagerServer(dip string, fmg *hootfs.FileManager,
+	vfmg *hootfs.VirtualFileManager) *HootFsServer {
+	return &HootFsServer{
+		dc:   *discover.NewDiscoverClient(dip),
+		fmg:  fmg,
+		vfmg: vfmg,
+	}
 }
 
 func (fms *HootFsServer) StartServer() error {
-    // First start server.
-    lis, err := net.Listen("tcp", headPort)
-    if err != nil {
+	// First start server.
+	lis, err := net.Listen("tcp", headPort)
+	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 
-    protos.RegisterHootFsServiceServer(s, fms)
+	protos.RegisterHootFsServiceServer(s, fms)
 
-    // Join the discovery server.
-    nodeId, clusterMap, err := fms.dc.JoinCluster()
+	// Join the discovery server.
+	nodeId, clusterMap, err := fms.dc.JoinCluster()
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    fms.csc = *cluster.NewClusterServiceClient(nodeId)
-    fms.csc.UpdateNodes(clusterMap)
+	fms.csc = *cluster.NewClusterServiceClient(nodeId)
+	fms.csc.UpdateNodes(clusterMap)
 
-    // Ping function.
-    go func() {
-        for {
-            time.Sleep(nodePingDurr * time.Second)
-            err := fms.dc.Ping()
+	// Ping function.
+	go func() {
+		for {
+			time.Sleep(nodePingDurr * time.Second)
+			err := fms.dc.Ping()
 
-            if err != nil {
-                // Error case!
-                // Not sure what to do here
-                // if we cannot ping the discovery 
-                // server.
-            }
-        }
-    }()
+			if err != nil {
+				// Error case!
+				// Not sure what to do here
+				// if we cannot ping the discovery
+				// server.
+			}
+		}
+	}()
 
-    // Get Active update function.
-    go func() {
-        for {
-            time.Sleep(nodeGetActiveDurr * time.Second)
-            clusterMap, err := fms.dc.GetActive()
+	// Get Active update function.
+	go func() {
+		for {
+			time.Sleep(nodeGetActiveDurr * time.Second)
+			clusterMap, err := fms.dc.GetActive()
 
-            if err != nil {
-                // TODO
-            } else {
-                fms.csc.UpdateNodes(clusterMap)
-            }
-        }
-    }()
+			if err != nil {
+				// TODO
+			} else {
+				fms.csc.UpdateNodes(clusterMap)
+			}
+		}
+	}()
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
 
-    // This should never be reached since Serve is blocking.
-    return nil
+	// This should never be reached since Serve is blocking.
+	return nil
 }
 
 func (s *HootFsServer) GetDirectoryContents(
 	ctx context.Context, request *head.GetDirectoryContentsRequest) (*head.GetDirectoryContentsResponse, error) {
-    
-
 	return nil, status.Error(codes.Unimplemented, "Method not implemented")
 }
 
 func (s *HootFsServer) MakeDirectory(
 	ctx context.Context, request *head.MakeDirectoryRequest) (*head.MakeDirectoryResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "Method not implemented")
+	parentUuid, err := uuid.FromBytes(request.DirId.Value)
+
+	if err != nil {
+		return nil, err
+	}
+
+	dirUuid, err := s.vfmg.CreateNewDirectory(request.DirName, parentUuid)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO : Must broadcast to all clients to make directory.
+
+	return &head.MakeDirectoryResponse{
+		DirId: &protos.UUID{
+			Value: dirUuid[:],
+		},
+	}, nil
 }
 
 func (s *HootFsServer) AddNewFile(
@@ -138,5 +155,3 @@ func (s *HootFsServer) RemoveObject(
 	ctx context.Context, request *head.RemoveObjectRequest) (*head.RemoveObjectResponse, error) {
 	return nil, status.Error(codes.Unimplemented, "Method not implemented")
 }
-
-
