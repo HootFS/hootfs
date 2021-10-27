9811f24581995238d107167047f600c9f0a97b2e
Joshua Washington <josh8551021@gmail.com>
2021-10-26 21:49:13 -0500

    Add system mapper and (incomplete) tests

---
+++ src/core/file_storage/system_mapper.go
@@ -0,0 +1,44 @@
+package hootfs
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+type SystemMapper struct {
+	systems       map[uint64]bool
+	systemMapping map[uuid.UUID][]uint64
+}
+
+var errFileAlreadyExistsInMapping = errors.New("File already exists in System mapper")
+
+func NewSystemMapper() *SystemMapper {
+	return &SystemMapper{systems: make(map[uint64]bool), systemMapping: make(map[uuid.UUID][]uint64)}
+}
+
+func (s *SystemMapper) AddSystem(sysId uint64) {
+	s.systems[sysId] = true
+}
+
+func (s *SystemMapper) RemoveSystem(sysId uint64) {
+	s.systems[sysId] = false
+}
+
+func (s *SystemMapper) MapNewFile(fileId uuid.UUID) ([]uint64, error) {
+	if _, exists := s.systemMapping[fileId]; exists {
+		return nil, errFileAlreadyExistsInMapping
+	}
+
+	s.systemMapping[fileId] = make([]uint64, len(s.systems))
+	for k := range s.systems {
+		s.systemMapping[fileId] = append(s.systemMapping[fileId], k)
+	}
+
+	return nil, nil
+}
+
+func (s *SystemMapper) RemoveFile(fileId uuid.UUID) error {
+	delete(s.systemMapping, fileId)
+	return nil
+}
