3a1c0f78834cf4b7e12e3d1de5bb02932d3acacf
Brandon Stanley <31254203+gamebot770@users.noreply.github.com>
2021-11-23 08:59:24 -0600

    Added in error message

--- quit.sh
+++ quit.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # Read a string with spaces using for loop
-kill $(ps aux | grep '[d]iscovery_main' | awk '{print $2}')
+processes=$(ps aux | grep '[d]iscovery_main' | awk '{print $2}')
+length=${#processes[@]}
+if [ -z "$processes" ]
+then
+	echo "No discovery_main processes to kill!"
+else
+	kill $processes
+fi
